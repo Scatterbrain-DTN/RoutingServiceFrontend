d5496ed56ccbd86a001a5a31354fa463e310ba84
Alex Ballmer <gnu3ra@riseup.net>
2021-02-13 13:58:32 -0600

    dynamically update button status based on discovery status

--- app/src/main/java/net/ballmerlabs/scatterbrain/ui/power/PowerFragment.java
+++ app/src/main/java/net/ballmerlabs/scatterbrain/ui/power/PowerFragment.java
@@ -1,6 +1,7 @@
     package net.ballmerlabs.scatterbrain.ui.power;
 
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
@@ -11,6 +12,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
@@ -30,31 +32,58 @@ import java.util.Random;
 public class PowerFragment extends Fragment {
     private static final String TAG = "PowerFragment";
     private ScatterbrainAPI binder;
+    private ToggleButton enableDisableRouterButton;
     private ServiceConnection mServiceConnection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
             binder = ScatterbrainAPI.Stub.asInterface(service);
             Log.v(TAG, "connected to ScatterRoutingService binder");
+            try {
+                binder.startDiscovery();
+                updateCheckedStatus();
+            } catch (RemoteException e) {
+                Log.e(TAG, "RemoteException: " + e);
+            }
         }
 
         @Override
         public void onServiceDisconnected(ComponentName componentName) {
+            updateCheckedStatus();
             binder = null;
         }
     };
+
+    private synchronized void updateCheckedStatus() {
+        if (enableDisableRouterButton == null) {
+            return;
+        }
+
+        try {
+            if (binder != null) {
+                enableDisableRouterButton.setChecked(binder.isDiscovering() || binder.isPassive());
+            }
+        } catch (RemoteException e) {
+            Log.e(TAG, "RemoteException: " + e);
+            enableDisableRouterButton.setChecked(false);
+        }
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_power, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
 
-        ToggleButton enableDisableRouterButton = root.findViewById(R.id.toggleButton);
-        enableDisableRouterButton.setChecked(false);
+        enableDisableRouterButton = root.findViewById(R.id.toggleButton);
+
+        final Intent bindIntent = new Intent(getActivity().getApplicationContext(), ScatterRoutingService.class);
+        getActivity().bindService(bindIntent, mServiceConnection, 0);
+        updateCheckedStatus();
         enableDisableRouterButton.setOnCheckedChangeListener((compoundButton, b) -> {
-            Intent startIntent = new Intent(getActivity().getApplicationContext(), ScatterRoutingService.class);
+            final Intent startIntent = new Intent(getActivity().getApplicationContext(), ScatterRoutingService.class);
             if (b) {
-                getActivity().startService(startIntent);
+                getActivity().startForegroundService(startIntent);
             } else {
+                getActivity().unbindService(mServiceConnection);
                 getActivity().stopService(startIntent);
             }
         });
