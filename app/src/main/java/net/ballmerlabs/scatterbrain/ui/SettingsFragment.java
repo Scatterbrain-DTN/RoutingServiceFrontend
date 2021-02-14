3ccd1d68f62d6e423cf16a465e2acdb2732e2b9c
Alex Ballmer <gnu3ra@riseup.net>
2021-02-14 00:38:31 -0600

    add scatterbrain settings

--- app/src/main/java/net/ballmerlabs/scatterbrain/ui/SettingsFragment.java
+++ app/src/main/java/net/ballmerlabs/scatterbrain/ui/SettingsFragment.java
@@ -1,7 +1,10 @@
 package net.ballmerlabs.scatterbrain.ui;
 
 import android.os.Bundle;
+import android.text.InputType;
+import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.preference.PreferenceFragmentCompat;
 
 import net.ballmerlabs.scatterbrain.R;
@@ -11,5 +14,12 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey);
+        for (String key : new String[]{"identity_share_cap", "message_share_cap"}) {
+            androidx.preference.EditTextPreference editTextPreference = getPreferenceManager().findPreference(key);
+            if (editTextPreference != null) {
+                editTextPreference.setOnBindEditTextListener(editText ->
+                        editText.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_SIGNED));
+            }
+        }
     }
 }
