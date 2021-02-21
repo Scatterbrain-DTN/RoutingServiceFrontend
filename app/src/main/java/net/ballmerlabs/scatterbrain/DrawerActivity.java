1f508d77c805b8581442fe681c8c10e979d4944d
Alex Ballmer <gnu3ra@riseup.net>
2021-02-20 21:09:20 -0600

    UI to grant location permission

--- app/src/main/java/net/ballmerlabs/scatterbrain/DrawerActivity.java
+++ app/src/main/java/net/ballmerlabs/scatterbrain/DrawerActivity.java
@@ -1,6 +1,9 @@
 package net.ballmerlabs.scatterbrain;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.PermissionInfo;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.MenuItem;
@@ -10,7 +13,10 @@ import android.view.Menu;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
+import com.sergivonavi.materialbanner.Banner;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -19,15 +25,44 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import java.security.Permission;
+
 public class DrawerActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
+    private Banner banner;
+
+    private ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    banner.dismiss();
+                } else {
+                    if (banner != null) {
+                        banner.setMessage(R.string.failed_location_text);
+                    }
+                }
+            });
+
+    private void requestForegroundLocation() {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawer);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        banner = findViewById(R.id.grantlocationbanner);
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_DENIED) {
+            banner.show();
+        }
+        banner.setRightButtonListener(view -> {
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION);
+        });
+
+        banner.setLeftButtonListener(view -> {
+            banner.setMessage(R.string.failed_location_text);
+        });
         setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -50,7 +85,7 @@ public class DrawerActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
     }
-    
+
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
