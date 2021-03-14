7fb7a519d508dc0c9ff801610a43e653c93dd9c5
Alex Ballmer <gnu3ra@riseup.net>
2021-03-14 01:23:37 -0500

    RoutingServiceViewModel observe identities and messages as livedata

--- app/src/main/java/net/ballmerlabs/scatterbrain/RoutingServiceViewModel.kt
+++ app/src/main/java/net/ballmerlabs/scatterbrain/RoutingServiceViewModel.kt
@@ -1,13 +1,23 @@
 package net.ballmerlabs.scatterbrain
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import dagger.hilt.android.lifecycle.HiltViewModel
 import net.ballmerlabs.scatterbrainsdk.Identity
+import net.ballmerlabs.scatterbrainsdk.ScatterMessage
 import javax.inject.Inject
 
 @HiltViewModel
 class RoutingServiceViewModel @Inject constructor(
         val repository: ServiceConnectionRepository
 ) : ViewModel() {
+    suspend fun observeMessages(application: String): LiveData<List<ScatterMessage>> {
+        return repository.observeMessages(application).asLiveData()
+    }
+
+    suspend fun observeIdentities() : LiveData<List<Identity>> {
+        return repository.observeIdentities().asLiveData()
+    }
 }
