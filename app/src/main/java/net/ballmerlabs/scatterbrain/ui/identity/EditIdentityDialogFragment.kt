23e0f04ecceeb4990e14f5f391fcdcbebe827018
Alex Ballmer <gnu3ra@riseup.net>
2021-03-20 11:16:33 -0400

    save autocompleted permissions in chip drawables

--- app/src/main/java/net/ballmerlabs/scatterbrain/ui/identity/EditIdentityDialogFragment.kt
+++ app/src/main/java/net/ballmerlabs/scatterbrain/ui/identity/EditIdentityDialogFragment.kt
@@ -1,16 +1,24 @@
 package net.ballmerlabs.scatterbrain.ui.identity
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
+import android.text.Spanned
+import android.text.style.ImageSpan
+import android.util.Log
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.MultiAutoCompleteTextView
 import android.widget.TextView
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.viewModelScope
+import com.google.android.material.chip.ChipDrawable
 import dagger.Binds
 import dagger.hilt.android.AndroidEntryPoint
 import net.ballmerlabs.scatterbrain.R
@@ -40,16 +48,45 @@ class EditIdentityDialogFragment : BottomSheetDialogFragment() {
     lateinit var adapter: ArrayAdapter<String>
     private val model: RoutingServiceViewModel by viewModels()
     
+    private fun createPermissionChip(name: String) {
+        val chip = ChipDrawable.createFromResource(requireContext(), R.xml.permission_chip)
+        val editText = binding.autocompleteAppSelector.text
+        val span = ImageSpan(chip)
+        chip.text = name
+        chip.setBounds(0, 0, chip.intrinsicWidth, chip.intrinsicHeight)
+        editText.setSpan(span, 0, editText.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+    }
+    
+    @SuppressLint("QueryPermissionsNeeded") //we declare the queries element
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
         binding = FragmentEditIdentityDialogListDialogBinding.inflate(inflater)
         identity = requireArguments().getParcelable(ARG_IDENTITY)!!
-        adapter = ArrayAdapter<String>(requireContext(), binding.autocompleteAppSelector.id)
-
+        adapter = ArrayAdapter<String>(requireContext(), android.R.layout.simple_dropdown_item_1line)
+        binding.editname.text = identity.givenname
+        binding.autocompleteAppSelector.setAdapter(adapter)
+        binding.autocompleteAppSelector.setTokenizer(MultiAutoCompleteTextView.CommaTokenizer())
+        binding.autocompleteAppSelector.threshold = 1
+        binding.autocompleteAppSelector.onItemClickListener = AdapterView.OnItemClickListener()
+        { _: AdapterView<*>, _: View, i: Int, _: Long ->
+            val str = adapter.getItem(i)
+            if (str != null) {
+                createPermissionChip(str)
+            } else {
+                Log.w(TAG, "attempted to create chip for null string at $i")
+            }
+        }
         model.viewModelScope.softCancelLaunch {
+            val apps = requireContext().packageManager.getInstalledApplications(0)
+            for (info in apps) {
+                if (info.name != null) {
+                    Log.v(TAG, "loading package: ${info.name}")
+                    adapter.add(info.name)
+                }
+            }
+            adapter.notifyDataSetChanged()
             model.getApplicationInfo(identity)
                     .observe(viewLifecycleOwner, { list ->
-                        adapter.clear()
                         for (info in list) {
                             adapter.add(info.name)
                         }
@@ -58,7 +95,12 @@ class EditIdentityDialogFragment : BottomSheetDialogFragment() {
         return binding.root
     }
 
+    private inner class AppPackageArrayAdapter(context: Context, resource: Int) : ArrayAdapter<String>(context, resource) {
+
+    }
+
     companion object {
+        const val TAG = "EditIdentityDialogFragment"
         fun newInstance(identity: Identity): EditIdentityDialogFragment =
                 EditIdentityDialogFragment().apply {
                     arguments = Bundle().apply {
