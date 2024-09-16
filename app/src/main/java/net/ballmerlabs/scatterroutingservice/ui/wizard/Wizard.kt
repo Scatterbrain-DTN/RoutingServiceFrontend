a0212b9d7ba7ec20c3e5bf2bd227a46bed62bbb9
fmeef <gnu3ra@riseup.net>
2024-09-15 22:22:49 -0500

    fancy animation

--- app/src/main/java/net/ballmerlabs/scatterroutingservice/ui/wizard/Wizard.kt
+++ app/src/main/java/net/ballmerlabs/scatterroutingservice/ui/wizard/Wizard.kt
@@ -7,10 +7,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -19,8 +22,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -28,6 +34,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import net.ballmerlabs.scatterroutingservice.R
 import net.ballmerlabs.uscatterbrain.dataStore
 
 const val SHOW_WIZARD = "show-wizard"
@@ -52,62 +59,66 @@ fun FirstStartWizard(
 
     if (show && model.states.isNotEmpty()) {
         Surface {
-
             val pagerState = rememberPagerState(pageCount = { model.states.size })
-
             HorizontalPager(state = pagerState) { page ->
                 val state = model.states[page]
-                Column(modifier = modifier, verticalArrangement = Arrangement.SpaceBetween) {
-                    val scrollstate = rememberScrollState()
-                    Column(
-                        modifier = Modifier.scrollable(
-                            scrollstate,
-                            orientation = Orientation.Vertical
-                        )
-                    ) {
-                        Text(
-                            color = MaterialTheme.colorScheme.onBackground,
-                            style = MaterialTheme.typography.headlineLarge,
-                            text = state.title
-                        )
-                        state.body()
-                    }
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceBetween
-                    ) {
-
-                        if (page > 0) {
-                            TextButton(
-                                onClick = {
-                                    scope.launch(Dispatchers.IO) {
-                                        pagerState.animateScrollToPage(page - 1)
-                                    }
-                                }) { Text("Previous") }
-                        } else {
-                            Column { }
+                Row(horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically,) {
+                    val arrowWidth = 15.dp
+                    val arrowHeight = 40.dp
+                    Icon(modifier = Modifier.size(arrowWidth, arrowHeight), painter =  painterResource(R.drawable.baseline_keyboard_double_arrow_left_24), contentDescription = "next")
+                    Column(modifier = modifier.weight(1F).padding(start = 4.dp), verticalArrangement = Arrangement.SpaceBetween) {
+                        val scrollstate = rememberScrollState()
+                        Column(
+                            modifier = Modifier.scrollable(
+                                scrollstate,
+                                orientation = Orientation.Vertical
+                            )
+                        ) {
+                            Text(
+                                color = MaterialTheme.colorScheme.onBackground,
+                                style = MaterialTheme.typography.headlineLarge,
+                                text = state.title
+                            )
+                            state.body()
                         }
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
 
-                        if (state.battery && !model.batteryState.value) {
-                            Button(onClick = model.onBattery) {
-                                Text("Grant battery permission")
-                            }
-                        } else if (model.batteryState.value && state.battery) {
-                            Text("Granted!")
-                        }
-                        state.Button(onClick = {
-                            scope.launch(Dispatchers.IO) {
-                                if (page < pagerState.pageCount - 1)
-                                    pagerState.animateScrollToPage(page + 1)
-                                else
-                                    withContext(Dispatchers.IO) {
-                                        context.dataStore.edit { p ->
-                                            p[booleanPreferencesKey(SHOW_WIZARD)] = false
+                            if (page > 0) {
+                                TextButton(
+                                    onClick = {
+                                        scope.launch(Dispatchers.IO) {
+                                            pagerState.animateScrollToPage(page - 1)
                                         }
-                                    }
+                                    }) { Text("Previous") }
+                            } else {
+                                Column { }
                             }
-                        })
+
+                            if (state.battery && !model.batteryState.value) {
+                                Button(onClick = model.onBattery) {
+                                    Text("Grant battery permission")
+                                }
+                            } else if (model.batteryState.value && state.battery) {
+                                Text("Granted!")
+                            }
+                            state.Button(onClick = {
+                                scope.launch(Dispatchers.IO) {
+                                    if (page < pagerState.pageCount - 1)
+                                        pagerState.animateScrollToPage(page + 1)
+                                    else
+                                        withContext(Dispatchers.IO) {
+                                            context.dataStore.edit { p ->
+                                                p[booleanPreferencesKey(SHOW_WIZARD)] = false
+                                            }
+                                        }
+                                }
+                            })
+                        }
                     }
+                    Icon(modifier = Modifier.size(arrowWidth, arrowHeight), painter =  painterResource(R.drawable.baseline_keyboard_double_arrow_right_24), contentDescription = "next")
                 }
             }
         }
--- app/src/main/java/net/ballmerlabs/scatterroutingservice/ui/wizard/WizardViewModel.kt
+++ app/src/main/java/net/ballmerlabs/scatterroutingservice/ui/wizard/WizardViewModel.kt
@@ -4,16 +4,39 @@ import android.Manifest
 import android.content.Context
 import android.os.Build
 import android.os.PowerManager
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.TweenSpec
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.AndroidViewModel
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.PermissionState
@@ -25,6 +48,12 @@ import net.ballmerlabs.scatterroutingservice.R
 import net.ballmerlabs.scatterroutingservice.ScatterbrainApp
 import net.ballmerlabs.scatterroutingservice.ui.PermissionSingleDialog
 import javax.inject.Inject
+
+@Composable
+fun Int.pxToDp() = with(LocalDensity.current) {
+    this@pxToDp.toDp()
+}
+
 
 data class WizardPermission constructor(
     val permissionState: String,
@@ -86,7 +115,76 @@ class WizardViewModel @Inject constructor(
     val states = mutableStateListOf(
         WizardState(
             title = "Welcome to Scatterbrain!",
-            body = { Text(stringResource(R.string.big_description)) }
+            body = {
+                val trans = rememberInfiniteTransition("welcome")
+                var end by remember { mutableStateOf(IntSize.Zero) }
+                var start by remember { mutableStateOf(IntSize.Zero) }
+                var person by remember { mutableStateOf(IntSize.Zero) }
+                var showfile by remember { mutableStateOf(false) }
+                val endWalk = end.width.pxToDp().value- start.width.pxToDp().value - person.width.pxToDp().value
+                val walk by trans.animateFloat(
+                    initialValue = 0.dp.value,
+                    targetValue = endWalk,
+                    animationSpec = infiniteRepeatable(tween(5000), RepeatMode.Reverse),
+                    label = "walk"
+                )
+
+                if (walk in 0.toFloat()..0.5.toFloat())
+                    showfile = true
+                if (walk in (endWalk-0.5).toFloat()..endWalk)
+                    showfile = false
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Row(
+                        modifier = Modifier.weight(1F)
+                            .onGloballyPositioned { v -> end = v.size },
+                        horizontalArrangement = Arrangement.Start,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            modifier = Modifier.size(60.dp).onGloballyPositioned { v ->
+                                start = v.size
+                            },
+                            painter = painterResource(R.drawable.baseline_groups_24),
+                            contentDescription = "Group of people"
+                        )
+                        Row(
+                            modifier = Modifier.wrapContentWidth()
+                                .offset(x = walk.dp)
+                        ) {
+                            if (showfile) {
+                                Icon(
+                                    modifier = Modifier.size(15.dp),
+                                    painter = painterResource(R.drawable.baseline_insert_drive_file_24),
+                                    contentDescription = "file"
+                                )
+                            }
+                            Icon(
+                                modifier = Modifier.size(30.dp).onGloballyPositioned { v ->
+                                    person = v.size
+                                },
+                                painter = painterResource(R.drawable.baseline_person_outline_24),
+                                contentDescription = "person"
+                            )
+
+                        }
+                    }
+                    Icon(
+                        modifier = Modifier.size(60.dp),
+                        painter = painterResource(R.drawable.baseline_groups_24),
+                        contentDescription = "Group of people"
+                    )
+                }
+
+
+                Text(
+                    text = stringResource(R.string.big_description)
+                )
+            }
         ),
         WizardState(
             title = "Grant location permission",
@@ -116,7 +214,7 @@ class WizardViewModel @Inject constructor(
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
                 add(
                     WizardState(
-                        title = "Grant bluetooth scan permissions",
+                        title = ctx.getString(R.string.bluetooth_title),
                         body = { Text(stringResource(R.string.bluetooth_scan_description)) },
                         permissionState = WizardPermission(
                             permissionState = Manifest.permission.BLUETOOTH_SCAN,
@@ -126,7 +224,7 @@ class WizardViewModel @Inject constructor(
                 )
                 add(
                     WizardState(
-                        title = "Grant bluetooth advertise permission",
+                        title = ctx.getString(R.string.advertise_title),
                         body = { Text(stringResource(R.string.advertise_description)) },
                         permissionState = WizardPermission(
                             permissionState = Manifest.permission.BLUETOOTH_ADVERTISE,
@@ -136,7 +234,7 @@ class WizardViewModel @Inject constructor(
                 )
                 add(
                     WizardState(
-                        title = "Grant bluetooth connect permission",
+                        title = ctx.getString(R.string.connect_title),
                         body = { Text(stringResource(R.string.bluetooth_connect_description)) },
                         permissionState = WizardPermission(
                             permissionState = Manifest.permission.BLUETOOTH_CONNECT,
@@ -146,7 +244,7 @@ class WizardViewModel @Inject constructor(
                 )
                 add(
                     WizardState(
-                        title = "Optionally grant permission to ignore battery optimizations",
+                        title = ctx.getString(R.string.battery_title),
                         body = { Text(stringResource(R.string.battery_description)) },
                         battery = true
                     )
