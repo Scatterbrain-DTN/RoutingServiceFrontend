17a4c8026e658eff7c7a4d5e5ae4bfa30feda30d
fmeef <gnu3ra@riseup.net>
2025-11-03 00:08:28 -0600

    working le?

--- app/src/main/java/net/ballmerlabs/scatterroutingservice/ui/debug/DebugFragment.kt
+++ app/src/main/java/net/ballmerlabs/scatterroutingservice/ui/debug/DebugFragment.kt
@@ -1,21 +1,32 @@
 package net.ballmerlabs.scatterroutingservice.ui.debug
 
 import android.net.Uri
+import android.widget.Spinner
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -28,6 +39,8 @@ import net.ballmerlabs.scatterroutingservice.RoutingServiceViewModel
 fun DatastoreBackup() {
     val scope = rememberCoroutineScope()
     val model: RoutingServiceViewModel = hiltViewModel()
+    var rebuilding by remember { mutableStateOf(false) }
+    var dismiss by remember { mutableStateOf(false) }
     val launcher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.CreateDocument("application/x-sqlite3")
     ) { uri: Uri? ->
@@ -35,6 +48,23 @@ fun DatastoreBackup() {
             model.repository.dumpDatastore(uri)
         }
     }
+
+    if (rebuilding && !dismiss) {
+        AlertDialog(
+            title = { Text("Rebuilding merkle datastore") },
+            text = {
+                Column {
+                    CircularProgressIndicator()
+                    Text("This process will continue in the background ")
+                }
+            },
+            onDismissRequest = { dismiss = true },
+            confirmButton = {
+                Button(onClick = { dismiss = true }) { Text("Continue in background") }
+            }
+        )
+    }
+
 
     Row(
         modifier = Modifier
@@ -46,6 +76,18 @@ fun DatastoreBackup() {
         }) {
             Text(text = "Export database")
         }
+
+        if (!rebuilding)
+            Button(onClick = {
+                scope.launch {
+                    rebuilding = true
+                    dismiss = false
+                    model.repository.merkleRebuild()
+                    rebuilding = false
+                }
+            }) {
+                Text(text = "Rebuild merkle store")
+            }
     }
 }
 
@@ -73,7 +115,8 @@ fun DebugView() {
                                 containerColor = MaterialTheme.colorScheme.secondaryContainer,
                                 disabledContainerColor = MaterialTheme.colorScheme.secondaryContainer,
                                 contentColor = MaterialTheme.colorScheme.contentColorFor(
-                                    MaterialTheme.colorScheme.secondaryContainer),
+                                    MaterialTheme.colorScheme.secondaryContainer
+                                ),
                                 disabledContentColor = MaterialTheme.colorScheme.contentColorFor(
                                     MaterialTheme.colorScheme.secondaryContainer
                                 )
